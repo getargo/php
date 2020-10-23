dcc383b7a60efc41b46af554160fefff5a4a7e35
Paul M. Jones <pmjones88@gmail.com>
2020-10-23 10:04:05 -0500

    add .htaccess file at initialization

--- src/Infrastructure/Initialize.php
+++ src/Infrastructure/Initialize.php
@@ -3,10 +3,11 @@ declare(strict_types=1);
 
 namespace Argo\Infrastructure;
 
-use Argo\Domain\DateTime;
 use Argo\Domain\Config\Config;
 use Argo\Domain\Content\ContentLocator;
 use Argo\Domain\Content\Post\Post;
+use Argo\Domain\DateTime;
+use Argo\Domain\Storage;
 use Argo\Infrastructure\BuildFactory;
 
 class Initialize
@@ -15,16 +16,26 @@ class Initialize
         DateTime $dateTime,
         Config $config,
         ContentLocator $content,
+        Storage $storage,
         BuildFactory $buildFactory
     ) {
         $this->dateTime = $dateTime;
         $this->config = $config;
         $this->content = $content;
+        $this->storage = $storage;
         $this->buildFactory = $buildFactory;
     }
 
     public function __invoke()
     {
+        $text = [
+            'Header set Cache-Control "no-cache, no-store, must-revalidate, max-age=0"',
+            'Header set Expires "0"',
+            'Header set Pragma "no-cache"',
+        ];
+
+        $this->storage->write('.htaccess', implode("\n", $text));
+
         $date = $this->dateTime->ymd();
         $relId = "{$date}/sample-post";
         $post = new Post(
