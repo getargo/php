42b47f865e1874eb3f094a6f8aa1478f6b632eae
Paul M. Jones <pmjones88@gmail.com>
2020-10-24 09:13:55 -0500

    link to Argo in footer

--- resources/theme/default/templates/layout/footer.php
+++ resources/theme/default/templates/layout/footer.php
@@ -3,7 +3,7 @@
 
         <?= $this->render('templates', $this->config->theme->layout_footer_prepend ?? []) ?>
 
-        <p>Built with Argo.</p>
+        <p>Built with <?= $this->anchor('https://github.com/getargo/app', 'Argo'); ?></p>
 
         <?= $this->render('templates', $this->config->theme->layout_footer_append ?? []) ?>
 
