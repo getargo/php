5074db74662158ce9c5b71345c0c77010b95154d
Paul M. Jones <pmjones88@gmail.com>
2020-11-27 13:00:15 -0600

    fix indent

--- resources/theme/default/templates/layout/body/main/sidebar.php
+++ resources/theme/default/templates/layout/body/main/sidebar.php
@@ -1,5 +1,5 @@
 <aside class="col-md-3">
     <?php foreach ($this->config->theme->sidebar as $widget): ?>
-            <?= $this->widget($widget); ?>
+        <?= $this->widget($widget); ?>
     <?php endforeach; ?>
 </aside>
